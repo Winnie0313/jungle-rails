2a121bae923ccd40a71b011df9d0be9486226c17
Winnie Li <liwanyi0313@gmail.com>
2022-09-21 22:13:03 +0000

    commit to create a new branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root to: 'products#index'
+  root to: 'products#index'  # request to homepage -> products controller -> call index method
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
