6a8eca855b75f371c91ad0dca49b45e4c15bee4d
Winnie Li <liwanyi0313@gmail.com>
2022-09-22 23:51:05 +0000

    added has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+  # validates :first_name, presence: true
+  # validates :last_price, presence: true
+  # validates :email, presence: true
+  # validates :password_confirmation, presence: true
 end
