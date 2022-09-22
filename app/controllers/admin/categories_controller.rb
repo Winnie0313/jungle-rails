88fac0de5d8ce08693bb030c72695f42de2f308c
Winnie Li <liwanyi0313@gmail.com>
2022-09-22 18:58:57 +0000

    retrieved categories data

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,41 @@
+class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
+
+  def index
+    @Categories = Category.order(id: :desc).all
+  end
+
+  def new
+    @Category = Category.new
+  end
+
+  # def create
+  #   @product = Product.new(product_params)
+
+  #   if @product.save
+  #     redirect_to [:admin, :products], notice: 'Product created!'
+  #   else
+  #     render :new
+  #   end
+  # end
+
+  # def destroy
+  #   @product = Product.find params[:id]
+  #   @product.destroy
+  #   redirect_to [:admin, :products], notice: 'Product deleted!'
+  # end
+
+  # private
+
+  # def product_params
+  #   params.require(:product).permit(
+  #     :name,
+  #     :description,
+  #     :category_id,
+  #     :quantity,
+  #     :image,
+  #     :price
+  #   )
+  # end
+
+end
