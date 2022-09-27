ae406d640c69d165dce1dc712b88e66f3d092a12
Winnie Li <liwanyi0313@gmail.com>
2022-09-27 05:32:48 +0000

    added validation for creating user and authenticate_with_credentials method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,14 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :first_name, presence: true
-  validates :last_price, presence: true
+  validates :last_name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 5 }
   validates :password_confirmation, presence: true, length: { minimum: 5 }
   # check if passwork and password_confirmation are the same
-  validates :password, confirmation
+  validates :password, confirmation: true
 
-  def authenticate_with_credentials(email_address, password)
+  def self.authenticate_with_credentials(email_address, password)
     user = User.find_by(email: email_address)
     if user && user.authenticate(password)
       user
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,176 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it "successfully creates user if all the fields are precent and valid" do 
+      @user = User.new(
+        first_name: "Winnie",
+        last_name: "Li",
+        email: "abc@gmail.com",
+        password: "123456",
+        password_confirmation: "123456"
+      )
+      @user.validate
+      expect(@user.errors.full_messages).to be_empty
+    end 
+
+    it "failed to create user when email is blank" do 
+      @user = User.new(
+        first_name: "Winnie",
+        last_name: "Li",
+        email: nil,
+        password: "123456",
+        password_confirmation: "123456"
+      )
+      @user.validate
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end 
+
+    it "failed to create user when first name is blank" do 
+      @user = User.new(
+        first_name: nil,
+        last_name: "Li",
+        email: "abc@gmail.com",
+        password: "123456",
+        password_confirmation: "123456"
+      )
+      @user.validate
+      expect(@user.errors.full_messages).to include("First name can't be blank")
+    end 
+
+    it "failed to create user when last name is blank" do 
+      @user = User.new(
+        first_name: "Winnie",
+        last_name: nil,
+        email: "abc@gmail.com",
+        password: "123456",
+        password_confirmation: "123456"
+      )
+      @user.validate
+      expect(@user.errors.full_messages).to include("Last name can't be blank")
+    end 
+
+    it "failed to create user when password is blank" do 
+      @user = User.new(
+        first_name: "Winnie",
+        last_name: "Li",
+        email: "abc@gmail.com",
+        password: nil,
+        password_confirmation: "123456"
+      )
+      @user.validate
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+    end 
+
+    it "failed to create user when password confirmation is blank" do 
+      @user = User.new(
+        first_name: "Winnie",
+        last_name: "Li",
+        email: "abc@gmail.com",
+        password: "123456",
+        password_confirmation: nil
+      )
+      @user.validate
+      expect(@user.errors.full_messages).to include("Password confirmation can't be blank")
+    end 
+
+    it "failed to create user when password and password confirmation don't match" do 
+      @user = User.new(
+        first_name: "Winnie",
+        last_name: "Li",
+        email: "abc@gmail.com",
+        password: "123456",
+        password_confirmation: "abcdefg"
+      )
+      @user.validate
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end 
+
+    it "failed to create user when password length is less than the minimum length" do 
+      @user = User.new(
+        first_name: "Winnie",
+        last_name: "Li",
+        email: "abc@gmail.com",
+        password: "1234",
+        password_confirmation: "1234"
+      )
+      @user.validate
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 5 characters)")
+    end 
+
+    it "failed to create user when email is not unique" do 
+      @user_1 = User.new(
+        first_name: "Winnie",
+        last_name: "Li",
+        email: "abc@gmail.com",
+        password: "123456",
+        password_confirmation: "123456"
+      )
+
+      @user_1.save
+
+      @user_2 = User.new(
+        first_name: "Jimmy",
+        last_name: "Lin",
+        email: "abc@gmail.com",
+        password: "abcdefg",
+        password_confirmation: "abcdefg"
+      )
+
+      @user_2.validate
+      expect(@user_2.errors.full_messages).to include("Email has already been taken")
+    end 
+
+
+
+  
+
+
+ 
+  end
+
+
+  
+  describe '.authenticate_with_credentials' do
+    before do
+      @user = User.new(
+        first_name: "Winnie",
+        last_name: "Li",
+        email: "abc@gmail.com",
+        password: "123456",
+        password_confirmation: "123456"
+      )
+
+      @user.save
+    end 
+
+    it "successfully login when successfully authenticated" do
+      user1 = User.authenticate_with_credentials("abc@gmail.com", "123456")
+      expect(user1).to be_truthy
+    end
+
+    it "successfully login if a visitor types in the wrong case for email" do
+      user1 = User.authenticate_with_credentials("ABC@GMAIL.COM", "123456")
+      expect(user1).to be_truthy
+    end
+
+    it "successfully login if a visitor types in a few spaces before and/or after their email address" do
+      user1 = User.authenticate_with_credentials("   abc@gmail.com", "123456")
+      expect(user1).to be_truthy
+    end
+
+    it "returns nil when entered wrong password" do 
+      expect(User.authenticate_with_credentials("abc@gmail.com", "wrong password")).to be_nil
+    end 
+
+    it "returns nil when entered wrong email" do 
+      expect(User.authenticate_with_credentials("wrong email", "123456")).to be_nil
+    end 
+
+
+
+
+
+    
+  end
 end
