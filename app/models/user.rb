ef6a07c787fedf932fcac64601ca6709c045e396
Winnie Li <liwanyi0313@gmail.com>
2022-09-27 01:52:18 +0000

    modified the create method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,18 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :first_name, presence: true, uniqueness: { case_sensitive: false }
+  validates :first_name, presence: true
   validates :last_price, presence: true
-  validates :email, presence: true
-  validates :password, presence: true, length: { is: 5 }
-  validates :password_confirmation, presence: true, length: { is: 5 }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 5 }
+  validates :password_confirmation, presence: true, length: { minimum: 5 }
   # check if passwork and password_confirmation are the same
-  validates :password, confirmation: { case_sensitive: ture }
+  validates :password, confirmation
 
   def authenticate_with_credentials(email_address, password)
-    @user = User.find_by(email: email_address)
-    if @user
-      @user
+    user = User.find_by(email: email_address)
+    if user && user.authenticate(password)
+      user
     else 
       nil
     end 
