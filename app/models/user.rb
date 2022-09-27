9a8f7282d3f4e84cb2b2884580352ff6706c7fdd
Winnie Li <liwanyi0313@gmail.com>
2022-09-27 01:11:14 +0000

    modified validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
-  # validates :first_name, presence: true
-  # validates :last_price, presence: true
-  # validates :email, presence: true
-  # validates :password_confirmation, presence: true
+
+  validates :first_name, presence: true, uniqueness: { case_sensitive: false }
+  validates :last_price, presence: true
+  validates :email, presence: true
+  validates :password, presence: true, length: { is: 5 }
+  validates :password_confirmation, presence: true, length: { is: 5 }
+  # check if passwork and password_confirmation are the same
+  validates :password, confirmation: { case_sensitive: ture }
+
 end
