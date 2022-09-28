cbc3cdca2b54adf6462609a18844c3e930b1ba5f
Winnie0313 <liwanyi0313@gmail.com>
2022-09-27 21:56:38 -0700

    created tests for homepage

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,14 +1,18 @@
-describe('example to-do app', () => {
+describe('hompage', () => {
   beforeEach(() => {
     // Cypress starts out with a blank slate for each test
     // so we must tell it to visit our website with the `cy.visit()` command.
     // Since we want to visit the same URL at the start of all our tests,
     // we include it in our beforeEach function so that it runs before each test
-    cy.visit('https://example.cypress.io/todo')
+    cy.visit('http://localhost:3000')
   })
 
   it("There is products on the page", () => {
     cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
   });
 
 })
